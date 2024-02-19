44f9071f6047c731e3c5a311200e4a4288aacf8f
zainabyahya <zainabh_2015@yahoo.com>
2024-02-19 16:47:36 +0300

    make code  more effiecent

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 let userPoints = 0, computerPoints = 0;
 
-function compareChoices(userChoice, computerChoice) {
-    if (userChoice === "rock") {
+function determineWinner(userChoice, computerChoice) {
+    if (computerChoice === userChoice) {
+        console.log("It's a tie! Let's play again.");
+        startGame();
+    } else if (userChoice === "rock") {
         if (computerChoice == "scissors") {
             userPoints++;
             console.log("You Win!");
         } else if (computerChoice === "paper") {
             computerPoints++;
             console.log("Computer Wins :(");
-        } else {
-            console.log("It's a tie! Let's play again.");
-            startGame();
         }
     } else if (userChoice === "paper") {
         if (computerChoice == "scissors") {
@@ -19,9 +19,6 @@ function compareChoices(userChoice, computerChoice) {
         } else if (computerChoice === "rock") {
             userPoints++;
             console.log("You Win!");
-        } else {
-            console.log("It's a tie! Let's play again.");
-            startGame();
         }
     } else if (userChoice === "scissors") {
         if (computerChoice == "rock") {
@@ -30,9 +27,6 @@ function compareChoices(userChoice, computerChoice) {
         } else if (computerChoice === "paper") {
             userPoints++;
             console.log("You Win!");
-        } else {
-            console.log("It's a tie! Let's play again.");
-            startGame();
         }
     } else {
         alert("Please enter a valid choice.")
@@ -44,7 +38,7 @@ function startGame() {
     let options = ["rock", "paper", "scissors"];
     let computerChoice = options[Math.floor(Math.random() * 3)];
     console.log(`Computer chose ${computerChoice}`);
-    compareChoices(userInput, computerChoice)
+    determineWinner(userInput, computerChoice)
 
 }
 
